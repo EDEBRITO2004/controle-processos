ad880d0b619bc50fba3bf40151092674026bc114
EDEBRITO2004 <ede.brito@outlook.com.br>
2026-09-07 11:43:36 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
 # -*- coding: utf-8 -*-
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/", response_class=HTMLResponse)
+async def read_root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+    
 import os
 from datetime import date
 from fastapi import FastAPI, Form
